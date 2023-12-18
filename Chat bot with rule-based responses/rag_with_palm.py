84fbfba25301e9ce62f5937cee43a74354ab1785
Thusharahan <145958270+Thusharahan@users.noreply.github.com>
2023-12-18 20:36:58 +0530

    Update rag_with_palm.py

--- Chat bot with rule-based responses/rag_with_palm.py
+++ Chat bot with rule-based responses/rag_with_palm.py
@@ -16,7 +16,7 @@ class RAGPaLMQuery:
         self.documents = SimpleDirectoryReader("./data").load_data()
 
         # Set up API key for PaLM
-        os.environ['GOOGLE_API_KEY'] = 'AIzaSyAoWU932Krc8MCUA28TEz1srQ4pfnXB5u8'
+        os.environ['GOOGLE_API_KEY'] = 'API key'
 
         # Initialize PaLM and Hugging Face embedding model
         self.llm = PaLM()
